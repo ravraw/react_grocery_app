8d34d481b394ca7a505f1c65849d92126d2c1f94
ravindra rawat <ravinrawat.wd@gmail.com>
2018-03-21 22:56:55 +0000

    add and change handler working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
+import NewItem from "./components/NewItem";
+import ItemList from "./components/ItemList";
+
 
 import './App.css';
 
 class App extends Component {
+  state = {
+    newItem:"",
+    itemList:[
+      {name: 'apple' ,bought: false},
+      {name: 'banana' ,bought: true},
+    ]
+  };
+
+  addItemHandler = (event) => {
+    event.preventDefault();
+    const newItem = {
+      name:this.state.newItem,
+      bought: false
+    }
+    const itemList = this.state.itemList;
+    itemList.push(newItem);
+    this.setState({itemList:itemList, newItem:""})
+    console.log(this.state);
+  }
+
+  onChangeHandler =(event) =>{
+    console.log(this.state.newItem);
+    this.setState({newItem: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
-        
+        <h1>React Grocery List App</h1>
+        <NewItem 
+          addItemHandler = {this.addItemHandler}
+          newItem = {this.state.newItem}
+          onChange = {this.onChangeHandler}/>
+        <ItemList 
+          ItemList = {this.state.itemList}
+          newItem = {this.state.newItem}/>
       </div>
     );
   }
